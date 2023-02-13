fd083f34519ac24a945e394996feaea141199abc
Kane Davidson <97858305+kcd1001@users.noreply.github.com>
2023-02-12 19:00:45 -0700

    Update CodeFormatter.c

--- CodeFormatter.c
+++ CodeFormatter.c
@@ -1,1 +1,23 @@
+//prototypes
 
+/*function takes in an input file name and appends OUT_FILE_STAMP. Then returns resulting string using parameter outString*/
+void createOutputFileName(const char *inString, char *outString)
+   {
+
+   }
+   
+/*function takes in pointer to input file, pointer to currentChar, pointer to lastChar, and iteration. Returns true if file has more data, false if file has ended. updates current and last char as return parameters*/   
+bool getNextChar(FILE *filePtr, char *currentChar, char *lastChar, int i )
+   {
+
+   }
+   
+bool isWhiteSpace(char chr )
+   {
+
+   }
+   
+bool writeFormattedFile(FILE *inputFilePtr, bool outputOpen, char *outFileName)
+   {
+
+   }

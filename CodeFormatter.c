43f5f0fba848178638f1361fc2bead2b47161dfb
bcu8 <bcu8@nau.edu>
2023-02-13 07:40:30 -0700

    Added dev enviornment added main fcn

--- CodeFormatter.c
+++ CodeFormatter.c
@@ -1,6 +1,81 @@
-//prototypes
+//prototypes=============================================================================
+#include "StandardConstants.h"
+#include "File_Input_Utility.h"
+#include "Console_IO_Utility.h"
+#include "File_Output_Utility.h"
 
-/*function takes in an input file name and appends OUT_FILE_STAMP. Then returns resulting string using parameter outString*/
+/*This program takes in a .c file of improperly formatted code then formats it 
+according to Professor Leverington's preference. The output file has the same name 
+as the input with "FORMATTED" appended to the original file name.
+
+This program will make the students life easier as they no longer have to worry about 
+how tabs will make their code look outside of the editor. 
+It also notifies student if there are lines exceeding 80 charactes so that they may 
+avoid those GUR reductions as well. 
+
+The graders experience will also become easier and more efficient.
+*/
+
+//constants==============================================================================
+const char *OUT_FILE_STAMP="FORMATTED.c";
+
+//prototypes=============================================================================
+
+/*function takes in an input file name and appends OUT_FILE_STAMP. Then returns 
+resulting string using parameter outString*/
+void createOutputFileName(const char *inString, char *outString);
+
+/*function takes in pointer to input file, pointer to currentChar, pointer to lastChar, and 
+iteration. Returns true if file has more data, false if file has ended. updates current 
+and last char as return parameters*/   
+bool getNextChar(FILE *filePtr, char *currentChar, char *lastChar, int i );
+
+//function to test if input char is a whitespace char
+bool isWhiteSpace(char chr );
+
+//function writes the formatted output file using GUR restrictions
+bool writeFormattedFile(FILE *inputFilePtr, bool outputOpen, char *outFileName);
+
+//begin main=============================================================================
+int main ()
+   {
+	//initialize variables
+	char fileName[40];
+	char outFileName[41];
+	FILE *inputFilePtr; 
+	bool outputOpen;
+	
+	//print title and purpose
+	printf("\n\nLeverington Safe Code Formatter\n");
+	printf("===============================\n\n");
+	
+	printf("This program takes in a file name, formats the code\nthen outputs the formatted code to a new .c file with\n\"FORMATTED\" appended to the original file name\n\n");
+	
+	//prompt for name of input file
+	printf("File Name (must end in .c): ");
+	getInputString(fileName, false );
+	printf("\n");
+	
+	//open input file 
+	inputFilePtr = fopen(fileName, "r");
+	
+	//create output file name
+	createOutputFileName(fileName, outFileName);
+	
+	//openOutput
+	outputOpen = openOutputFile(outFileName);
+	
+	//write formatted code to output file
+	writeFormattedFile(inputFilePtr, outputOpen, outFileName);
+	
+	//end main
+	return 0;
+   }
+
+//function implementation================================================================   
+   
+/*function takes in an input file name and appends OUT_FILE_STAMP. Then returns 
+resulting string using parameter outString*/
 void createOutputFileName(const char *inString, char *outString)
    {
 	strcpy(outString,inString);
@@ -10,7 +85,9 @@ void createOutputFileName(const char *inString, char *outString)
 	strcat(outString, OUT_FILE_STAMP);
    }
    
-/*function takes in pointer to input file, pointer to currentChar, pointer to lastChar, and iteration. Returns true if file has more data, false if file has ended. updates current and last char as return parameters*/   
+/*function takes in pointer to input file, pointer to currentChar, pointer to lastChar, 
+and iteration. Returns true if file has more data, false if file has ended. updates 
+current and last char as return parameters*/   
 bool getNextChar(FILE *filePtr, char *currentChar, char *lastChar, int i )
    {
 	//if not first iteration, update lastChar
---
+++ Development files/README.txt
@@ -0,0 +1,10 @@
+This folder contains every file that should be in a development directory
+ 
+It should include:
+	-codeFormatter.c
+	-makefile for compiling
+	-Leveringtons file utilities
+	-Leveringtons standard constants
+	-Leveringtons IO utilities
+	-Test files (.c files only)
+	-The current .exe compiled program
